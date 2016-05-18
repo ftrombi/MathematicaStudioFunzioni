bffe1a5e61ae67d2e1b661444d0986826c947dd9
Francesco Trombi <trombi92@gmail.com>
2016-05-18 09:41:42 +0200

    [FT-4] Aggiunto .m per calcolo del segno della derivata prima.

--- derivata.m
+++ derivata.m
@@ -30,9 +30,21 @@ BeginPackage["DerivataPrima`"]
 CalcoloDerivataPrima::usage =
 	"CalcoloDerivataPrima[f, x] calcola la derivata prima della funzione f."
 
+SegnoDerivataPrima::usage =
+	"SegnoDerivataPrima[f, x] restituisce il segno della derivata prima della funzione f."
+
 Begin["`Private`"]
 
 CalcoloDerivataPrima[f_, x_]:= Simplify[D[f[x], x]];
+
+SegnoDerivataPrima[f_, x_] := (
+  d = CalcoloDerivataPrima[f, x];
+  num = Numerator[d];
+  soluzioni = NSolve[num == 0, x];
+  punto = Reduce[d == 0, x];
+  Reduce[d > 0, x]
+  Reduce[d < 0, x]
+)
 
 End[]
 EndPackage[]
