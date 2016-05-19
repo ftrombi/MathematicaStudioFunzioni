cae5f9fb897f5a75dc6b1878225741cfe8740fe1
Francesco Trombi <trombi92@gmail.com>
2016-05-19 16:21:38 +0200

    [FT-4] Aggiunti passaggi del calcolo della derivata. Aggiunta stampa della corretta monotonia.

--- derivata.m
+++ derivata.m
@@ -27,6 +27,14 @@
 
 BeginPackage["DerivataPrima`"]
 
+StampaMonotonia::usage =
+	"StampaMonotonia[intervalliPositivi, intervalliNegativi] permette di stampare la monotonia
+	della funzione con una formattazione corretta"
+
+StampaIntervalli::usage =
+	"StampaIntervalli[listaIntervalliPositivi, listaIntervalliNegativi] permette di stampare
+	gli intervalli di monotonia della funzione con una formattazione corretta"
+
 CalcoloDerivataPrima::usage =
 	"CalcoloDerivataPrima[f, x] calcola la derivata prima della funzione f."
 
@@ -35,18 +43,35 @@ SegnoDerivataPrima::usage =
 
 Begin["`Private`"]
 
+StampaMonotonia[intervalliPositivi_, intervalliNegativi_] :=
+  If[Length[intervalliPositivi] > 0, 
+   Print["La funzione \[EGrave] monotona crescente"], 
+   Print["La funzione \[EGrave] monotona decrescente"]];
+
+StampaIntervalli[listaIntervalliPositivi_, listaIntervalliNegativi_]:=(
+	Print["Gli intervalli in cui la derivata \[EGrave] positiva sono:"];
+	Print[listaIntervalliPositivi];
+	Print["Gli intervalli in cui la derivata \[EGrave] negativa sono:"];
+	Print[listaIntervalliNegativi];
+
+);
+
 CalcoloDerivataPrima[f_, x_]:= Simplify[D[f[x], x]];
 
 SegnoDerivataPrima[f_, x_] := (
-  d = CalcoloDerivataPrima[f, x];
+    d = CalcoloDerivataPrima[f, x];
+	Print["La derivata prima della funzione \[EGrave]:"];
+	Print[d];
 	num = Numerator[d];
-	soluzioni = NSolve[num == 0, x];
-	punto = Reduce[d == 0, x];
-	segno1 = Reduce[d < 0, x];
-	segno2 = Reduce[d > 0, x];
-	If[StringCount[ToString[segno1], "False"] > 0 || 
-  StringCount[ToString[segno2], "False"] > 
-   0, "La funzione è monotona", segno1 segno2]
+	soluzioni = Solve[num == 0, x];
+	Print["Le soluzioni della derivata prima sono:"];
+	Print[Column[soluzioni]];
+	intervalliPositivi = Reduce[d > 0, x];
+	intervalliNegativi = Reduce[d < 0, x];
+	If[StringCount[ToString[intervalliPositivi], "False"] > 0 || 
+      StringCount[ToString[intervalliNegativi], "False"] > 0, 
+      StampaMonotonia[intervalliPositivi, intervalliNegativi], 
+      StampaIntervalli[intervalliPositivi, intervalliNegativi]];
 )
 
 End[]
