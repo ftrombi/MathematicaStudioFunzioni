0e122a3e2d4dcfc30106831bb91a10b191ce8168
Francesco Trombi <trombi92@gmail.com>
2016-05-20 16:22:16 +0200

    [FT-5] Aggiunto grafico alla derivata seconda; aggiunte funzioni per la stampa formattata dei risultati; gestite derivate seconde senza soluzioni.

--- derivata2.m
+++ derivata2.m
@@ -27,6 +27,22 @@
 
 BeginPackage["DerivatSeconda`"]
 
+StampaConcavoConvessoNoSoluzioni::usage =
+	"StampaConcavoConvessoNoSoluzioni[d2_] permette di stampare correttamente la concavit\[AGrave]
+	o la convessit\[AGrave] di una funzione con derivata seconda senza soluzioni."
+
+GraficoDerivataSeconda::usage =
+	"GraficaDerivataSeconda[d2_, x_, limite0_, limite1_] permette di eseguire un plot
+	della funzione tra gli intervalli limite0 e limite1"
+
+StampaConcavoConvesso::usage =
+	"StampaConcavoConvesso[intervalliConvessa_, intervalliConcava_] permette di
+	stampare la convessit\[AGrave] e la concavit\[AGrave] della funzione con una formattazione corretta."
+
+StampaIntervalli::usage =
+	"StampaIntervalli[intervalliConvessa_, intervalliConcava_] permette di stampare
+	gli intervalli di convessit\[AGrave] e concavit\[AGrave] della funzione con una formattazione corretta"
+
 CalcoloDerivataSeconda::usage =
 	"CalcoloDerivataSeconda[f, x] calcola la derivata seconda della funzione f."
 
@@ -35,18 +51,41 @@ SegnoDerivataSeconda::usage =
 
 Begin["`Private`"]
 
+StampaConcavoConvessoNoSoluzioni[d2_] := If[d2 > 0, Print["La funzione \[EGrave] convessa"],
+	Print["La funzione \[EGrave] concava"]];
+
+GraficoDerivataSeconda[d_, x_, limite0_, limite1_] := (
+	Print["Il grafico della derivata seconda \[EGrave] il seguente:"];
+	Plot[d, {x, -5,+5}]
+);
+
+StampaConcavoConvesso[intervalliConvessa_, intervalliConcava_] :=
+  If[Length[intervalliConvessa] > 0, 
+   Print["La funzione \[EGrave] convessa."], 
+   Print["La funzione \[EGrave] concava"]];
+
+StampaIntervalli[intervalliConvessa_, intervalliConcava_]:=(
+	Print["Gli intervalli in cui la derivata seconda \[EGrave] positiva e dunque la funzione \[EGrave] convessa sono:"];
+	Print[intervalliConvessa];
+	Print["Gli intervalli in cui la derivata seconda \[EGrave] negativa e dunque la funzione \[EGrave] concava sono:"];
+	Print[intervalliConcava];
+
+);
+
 CalcoloDerivataSeconda[f_, x_]:= D[D[f[x], x], x];
 
 SegnoDerivataSeconda[f_, x_] := (
-  d = CalcoloDerivataSeconda[f, x];
-	num = Numerator[d];
+    d2 = CalcoloDerivataSeconda[f, x];
+	num = Numerator[d2];
 	soluzioni = NSolve[num == 0, x];
-	punto = Reduce[d == 0, x];
-	segno1 = Reduce[d < 0, x];
-	segno2 = Reduce[d > 0, x];
-	If[StringCount[ToString[segno1], "False"] > 0 || 
-  	StringCount[ToString[segno2], "False"] > 0,
-  	"La funzione non cambia convessità", segno1 segno2]
+	intervalliConvessa = Reduce[d2 > 0, x];
+	intervalliConcava = Reduce[d2 < 0, x];
+	If[Length[intervalliConvessa] == 0 || Length[intervalliConcava] == 0,
+		StampaConcavoConvessoNoSoluzioni[d2], StampaIntervalli[intervalliConvessa, intervalliConcava]];
+	(*If[StringCount[ToString[intervalliConvessa], "False"] > 0 || 
+  	StringCount[ToString[intervalliConcava], "False"] > 0,
+  	StampaConcavoConvesso[intervalliConvessa, intervalliConcava],
+	  StampaIntervalli[intervalliConvessa, intervalliConcava]];*)
 )
 
 End[]
