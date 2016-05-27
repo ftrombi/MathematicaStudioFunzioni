bb2199850e8c5e984f794ae6a63c388c9178c048
Francesco Trombi <trombi92@gmail.com>
2016-05-27 11:35:25 +0200

    Applicate correzioni suggerite.

--- Relazione/studiofunzioni.m
+++ Relazione/studiofunzioni.m
@@ -4,13 +4,14 @@
 
 (* :Context: StudioFunzioni` *)
 
-(* :Author: Francesco Trombi e Sebastian Davrieux *)
+(* :Author: Sebastian Davrieux e Francesco Trombi
+	Informatica Magistrale *)
 
 (* :Summary:
    Insieme di funzioni per il supporto allo studio di funzioni nelle scuole superiori.
  *)
 
-(* :Copyright: Trombi e Davrieux *)
+(* :Copyright: Davrieux e Trombi, Matematica Computazionale, AA 2015/16, Mathematica v10.0.0.0 *)
 
 (* :Package Version:  *)
 
@@ -68,8 +69,11 @@ GraficoFunzione::usage =
 Begin["`Private`"]
 
 (* Dominio *)
+
+(* Funzione che verifica se il parametro x \[EGrave] razionale. *)
 VerificaRazionale[x_]:=If[Simplify[x\[Element]Rationals],True,False,Null];
 
+(* Funzione che estrae il radicando del parametro x solo se contenuto in una radice pari. *)
 PrendiContenutoRadice[x_]:=If[Head[x] ==Power,
 	If[VerificaRazionale[Part[x,2]],
 		If[EvenQ[Denominator[Part[x,2]]],Part[x,1],Null,Null], 
@@ -78,30 +82,45 @@ PrendiContenutoRadice[x_]:=If[Head[x] ==Power,
 	Null,
 	Null];
 
+(* Funzione che estrae il contenuto del logaritmo del parametro x. *)
 PrendiContenutoLogaritmo[x_]:=If[Head[x] ==Log,Part[x,1],Null,Null];
 
+(* Funzione che estrae il secondo contenuto del parametro x se l'head del parametro \[EGrave] Times[] *)
 PrendiSecondoContenutoSeHeadUgualeTimes[x_]:=If[Head[x] ==Times,Part[x,2],x,x];
 
+(* Funzione che crea una lista di operandi estratti dal parametro x, ignorando il denominatore. *)
 DividiFunzioneInListaDiOperandi[x_] :=If[Denominator[x]===1,
 	List @@ Numerator[x],
 	Join[List @@ Denominator[x], List@@Numerator[x]]];
 
+(* SetAttributes[] consente il passaggio per riferimento del primo parametro della funzione CalcolaContenutiRadiciDaControllare[]. *)
 SetAttributes[CalcolaContenutiRadiciDaControllare,HoldFirst];
+(* Funzione che genera una lista con tutti i radicandi del parametro x. *)
 CalcolaContenutiRadiciDaControllare[listaContenutiRadiciDaControllare_,x_]:=If[PrendiContenutoRadice[PrendiSecondoContenutoSeHeadUgualeTimes[x]] ===Null,
 	Null,
 	listaContenutiRadiciDaControllare=Append[listaContenutiRadiciDaControllare,PrendiContenutoRadice[PrendiSecondoContenutoSeHeadUgualeTimes[x]]]];
 
+(* SetAttributes[] consente il passaggio per riferimento del primo parametro della funzione CalcolaContenutiLogaritmoDaControllare[]. *)
 SetAttributes[CalcolaContenutiLogaritmoDaControllare,HoldFirst];
+(* Funzione che genera una lista con tutti i contenuti dei logaritmi del parametro x. *)
 CalcolaContenutiLogaritmoDaControllare[listaContenutiLogaritmoDaControllare_,x_]:=If[PrendiContenutoLogaritmo[PrendiSecondoContenutoSeHeadUgualeTimes[x]] ===Null,
 	Null,
 	listaContenutiLogaritmoDaControllare=Append[listaContenutiLogaritmoDaControllare,PrendiContenutoLogaritmo[PrendiSecondoContenutoSeHeadUgualeTimes[x]]]];
 
+(* SetAttributes[] consente il passaggio per riferimento del primo parametro della funzione CalcolaContenutiDenominatoreDaControllare[]. *)
 SetAttributes[CalcolaContenutiDenominatoreDaControllare,HoldFirst];
+(* Funzione che genera una lista con tutti i contenuti del denominatore del parametro x. *)
 CalcolaContenutiDenominatoreDaControllare[risultatoControlloDenominatore_,listaContenutiDenominatoreDaControllare_]:=(
 	risultatoControlloDenominatore=Union[risultatoControlloDenominatore,Reduce[#<0,x,Reals]&/@listaContenutiDenominatoreDaControllare];
 	risultatoControlloDenominatore=Union[risultatoControlloDenominatore,Reduce[#>0,x,Reals]&/@listaContenutiDenominatoreDaControllare];
 );
 
+(* Funzione che:
+	- divide la funzione in una lista di operandi;
+	- cerca i contenuti delle radici, dei logaritmi e del denominatore all'interno di essa;
+	- calcola il dominio utilizzando queste liste;
+	- per sicurezza, il dominio mostrato all'utente viene calcolato dalla funzione nativa FunctionDomain[].
+*)
 CalcolaDominio[f_,x_]:=(
 	fDivisa= DividiFunzioneInListaDiOperandi[f[x]];
 	listaContenutiRadiciDaControllare={};
@@ -144,6 +163,8 @@ CalcolaDominio[f_,x_]:=(
 );
 
 (* Pari e dispari *)
+
+(* Funzione che effettua il controllo della parit\[AGrave] e della disparit\[AGrave] di una funzione. *)
 ControlloPariDispari[f_, x_]:=(
 	fMenoX = f[x] /. {x->-x};
     risultatoPari = FullSimplify[ForAll[x,f[x] == fMenoX]];
@@ -157,6 +178,8 @@ ControlloPariDispari[f_, x_]:=(
 ); 
 
 (* Intersezione con gli assi *)
+
+(* Funzione che controlla la presenza di intersezione con gli assi, stampandone i risultati in caso di presenza. *)
 ControlloIntersezioneAssi[f_,x_]:=(
 	Print["Per scoprire dove interseca l'asse x poniamo il numeratore = 0: "];
 	Print[Numerator[f[x]]==0];
@@ -170,6 +193,8 @@ ControlloIntersezioneAssi[f_,x_]:=(
 );
 
 (* Segno funzione *)
+
+(* Funzione che valuta gli intervalli in cui la funzione \[EGrave] positiva. *)
 SegnoFunzione[f_, x_]:= (
 	valore = Reduce[f[x] > 0, x, Reals];
 	If[valore===False, Print["La funzione \[EGrave] negativa."],
@@ -177,11 +202,15 @@ SegnoFunzione[f_, x_]:= (
 ); 
 
 (* Calcola Limiti *)
+
+(* Funzione che controlla se il parametro var \[EGrave] una lista. *)
 ControlloSeLista[var_]:=(
 	If[ListQ[var],var,{var}]
 );
 
+(* SetAttributes[] consente il passaggio per riferimento del primo parametro della funzione CreazioneListaDominio[]. *)
 SetAttributes[CreazioneListaDominio,HoldFirst];
+(* Funzione che crea una lista contenente gli intervalli del dominio di definizione della funzione, definito utilizzando la FunctionDomain[]. *)
 CreazioneListaDominio[listaDominio_,dominio_]:=(listaDominio=Union[listaDominio,ControlloSeLista[Select[dominio,Head[#]==Greater&]]];
 	listaDominio=Union[listaDominio,ControlloSeLista[Select[dominio,Head[#]==GreaterEqual&]]];
 	listaDominio=Union[listaDominio,ControlloSeLista[Select[dominio,Head[#]==Inequality&]]];
@@ -190,7 +219,9 @@ CreazioneListaDominio[listaDominio_,dominio_]:=(listaDominio=Union[listaDominio,
 	listaDominio = DeleteCases[listaDominio,False];
 );
 
+(* SetAttributes[] consente il passaggio per riferimento del primo parametro della funzione CalcolaEstremiInferiori[]. *)
 SetAttributes[CalcolaEstremiInferiori,HoldFirst];
+(* Funzione che crea una lista contenente gli estremi inferiori del dominio di definizione della funzione formattato dalla funzione CreazioneListaDominio[]. *)
 CalcolaEstremiInferiori[estremiInferiori_,dominio_]:=(estremiInferiori=Union[estremiInferiori,#[[2]]&/@Select[dominio,Head[#]==Greater&]];
 	estremiInferiori=Union[estremiInferiori,#[[2]]&/@Select[dominio,Head[#]==GreaterEqual&]];
 	estremiInferiori=Union[estremiInferiori,If[#[[2]]===Less,#[[1]]]&/@Select[dominio,Head[#]==Inequality&]];
@@ -200,7 +231,9 @@ CalcolaEstremiInferiori[estremiInferiori_,dominio_]:=(estremiInferiori=Union[est
 	estremiInferiori=DeleteCases[estremiInferiori,Null];
 );
 
+(* SetAttributes[] consente il passaggio per riferimento del primo parametro della funzione CalcolaEstremiSuperiori[]. *)
 SetAttributes[CalcolaEstremiSuperiori,HoldFirst];
+(* Funzione che crea una lista contenente gli estremi superiori del dominio di definizione della funzione formattato dalla funzione CreazioneListaDominio[]. *)
 CalcolaEstremiSuperiori[estremiSuperiori_,dominio_]:=(estremiSuperiori=Union[estremiSuperiori,#[[2]]&/@Select[dominio,Head[#]==Less&]];
 	estremiSuperiori=Union[estremiSuperiori,#[[2]]&/@Select[dominio,Head[#]==LessEqual&]];
 	estremiSuperiori=Union[estremiSuperiori,If[#[[2]]===Less,#[[5]]]&/@Select[dominio,Head[#]==Inequality&]];
@@ -210,6 +243,13 @@ CalcolaEstremiSuperiori[estremiSuperiori_,dominio_]:=(estremiSuperiori=Union[est
 	estremiSuperiori=DeleteCases[estremiSuperiori,Null];
 );
 
+(* Funzione che:
+	- calcola il dominio della funzione presa in input;
+	- formatta gli intervalli inserendoli in una lista;
+	- calcola gli estremi inferiori e superiori;
+	- calcola i limiti su tali estremi;
+	- controlla il risultato per identificare asintoti.
+*)
 CalcolaLimiti[f_,x_]:=(
 	dominio=FunctionDomain[f[x],x];
 	dominio=If[Head[dominio]===Or,dominio,{dominio}];
@@ -241,16 +281,19 @@ CalcolaLimiti[f_,x_]:=(
 );
 
 (* Derivata Prima *)
+(* Funzione che permette di stampare in maniera formattata le soluzioni della derivata prima. *)
 StampaSoluzioni[soluzioni_] := (
 	Print["Le soluzioni della derivata prima sono:"];
 	Print[Column[soluzioni]];
 )
 
+(* Funzione che permette di stampare in maniera formattata la monotonia della funzione. *)
 StampaMonotonia[intervalliPositivi_, intervalliNegativi_] :=
   If[Length[intervalliPositivi] > 0, 
    Print["La funzione \[EGrave] monotona crescente"], 
    Print["La funzione \[EGrave] monotona decrescente"]];
 
+(* Funzione che permette di stampare in maniera formattata gli intervalli di monotonia della funzione primitiva. *)
 StampaIntervalli[listaIntervalliPositivi_, listaIntervalliNegativi_]:=(
 	Print["Gli intervalli in cui la derivata \[EGrave] positiva sono:"];
 	Print[listaIntervalliPositivi];
@@ -259,8 +302,16 @@ StampaIntervalli[listaIntervalliPositivi_, listaIntervalliNegativi_]:=(
 
 );
 
+(* Funzione che calcola la derivata prima della funzione. *)
 CalcoloDerivataPrima[f_, x_]:= Simplify[D[f[x], x]];
 
+(* Funzione che:
+	- calcola la derivata prima della funzione;
+	- stampa la derivata prima;
+	- controlla la presenza di soluzioni della derivata prima;
+	- determina gli intervalli di monotonia;
+	- stampa il grafico della derivata prima, utilizzando gli estremi del dominio della derivata prima per limitarlo.
+*)
 SegnoDerivataPrima[f_, x_] := (
     d = CalcoloDerivataPrima[f, x];
 	Print["La derivata prima della funzione \[EGrave]:"];
@@ -301,24 +352,30 @@ SegnoDerivataPrima[f_, x_] := (
 )
 
 (* Derivata Seconda *)
+
+(* Funzione che stampa in maniera formattata la convessit\[AGrave] e la concavit\[AGrave] di una funzione, se la derivata seconda \[EGrave] priva di soluzioni. *)
 StampaConcavoConvessoNoSoluzioni[d2_] := If[d2 > 0, Print["La funzione \[EGrave] convessa"],
 	Print["La funzione \[EGrave] concava"]];
 
+(* Funzione che stampa il grafico della derivata seconda. *)
 GraficoDerivataSeconda[d_, x_, limite0_, limite1_] := (
 	Print["Il grafico della derivata seconda \[EGrave] il seguente:"];
 	Plot[d, {x, -5,+5}]
 );
 
+(* Funzione che permette di stampare in maniera formattata le soluzioni della derivata seconda. *)
 StampaSoluzioniDerivataSeconda[soluzioni_] := (
 	Print["Le soluzioni della derivata seconda sono:"];
 	Print[Column[soluzioni]];
 )
 
+(* Funzione che stampa in maniera formattata la convessit\[AGrave] e la concavit\[AGrave] di una funzione. *)
 StampaConcavoConvesso[intervalliConvessa_, intervalliConcava_] :=
   If[Length[intervalliConvessa] > 0, 
    Print["La funzione \[EGrave] convessa."], 
    Print["La funzione \[EGrave] concava"]];
 
+(* Funzione che stampa in maniera formattata gli intervalli di convessit\[AGrave] e concavit\[AGrave] della funzione primitiva. *)
 StampaIntervalliDerivataSeconda[intervalliConvessa_, intervalliConcava_]:=(
 	Print["Gli intervalli in cui la derivata seconda \[EGrave] positiva e dunque la funzione \[EGrave] convessa sono:"];
 	Print[intervalliConvessa];
@@ -327,8 +384,16 @@ StampaIntervalliDerivataSeconda[intervalliConvessa_, intervalliConcava_]:=(
 
 );
 
+(* Funzione che permette di calcolare la derivata seconda della funzione. *)
 CalcoloDerivataSeconda[f_, x_]:= D[D[f[x], x], x];
 
+(* Funzione che:
+	- calcola la derivata seconda della funzione;
+	- stampa la derivata seconda;
+	- controlla la presenza di soluzioni della derivata seconda;
+	- determina gli intervalli di convessit\[AGrave] e concavit\[AGrave];
+	- stampa il grafico della derivata seconda, utilizzando gli estremi del dominio della derivata seconda per limitarlo.
+*)
 SegnoDerivataSeconda[f_, x_] := (
     d2 = CalcoloDerivataSeconda[f, x];
 	Print["La derivata seconda \[EGrave]"];
@@ -368,6 +433,8 @@ SegnoDerivataSeconda[f_, x_] := (
 )
 
 (* Grafico *)
+
+(* Funzione che stampa il grafico della funzione, utilizzando gli estremi del dominio per limitarlo. *)
 GraficoFunzione[f_, x_]:=(
 	Print["Il grafico della funzione \[EGrave]: "];
 	dominio=FunctionDomain[f[x],x];
